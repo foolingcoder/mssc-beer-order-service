39a3e4ed3d3939bb7d078e8756dc92c072092fea
angump <angump@host.docker.internal>
2020-03-23 09:26:15 +0400

    Added state machine action components, beer state interceptor and beer service manager

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -1,10 +1,16 @@
 package guru.sfg.beer.order.service.config;
 
+import javax.jms.Destination;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import guru.sfg.beer.order.service.sm.action.ValidateOrderAction;
+import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 public class JmsConfig {
@@ -12,6 +18,16 @@ public class JmsConfig {
     public static final String MY_QUEUE = "MY_QUEUE";
     
     public static final String MY_SEND_RCV_QUEUE = "MY_SEND_RCV_QUEUE";
+
+	public static final String VALIDATE_ORDER_QUEUE = "VALIDATE_ORDER_QUEUE";
+	
+	public static final String ALLOCATE_ORDER_QUEUE = "ALLOCATE_ORDER_QUEUE";
+
+	public static final String ALLOCATE_FAILURE_QUEUE = "ALLOCATE_FAILURE_QUEUE";
+
+	public static final String DEALLOCATE_ORDER_QUEUE = "DEALLOCATE_ORDER_QUEUE";
+
+	public static final String  VALIDATE_FAILURE_QUEUE = "VALIDATE_FAILURE_QUEUE";
 
     @Bean
     public MessageConverter messageConverter(){
---
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -0,0 +1,69 @@
+package guru.sfg.beer.order.service.services;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.statemachine.support.StateMachineInterceptor;
+import org.springframework.stereotype.Service;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BeerOrderManagerImpl implements BeerOrderManager {
+	
+	public static String ORDER_ID_HEADER ="beer_order_id";
+
+	private final BeerOrderRepository beerRepository;
+
+	private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
+
+	private StateMachineInterceptor<BeerOrderStatusEnum, BeerOrderEventEnum> beerOrderStateChangeInterceptor;
+
+	@Override
+	public BeerOrder newBeerOrder(BeerOrder beerOrder) {
+		beerOrder.setId(null);
+		beerOrder.setOrderStatus(BeerOrderStatusEnum.NEW);
+
+		BeerOrder savedBeerOrder = beerRepository.save(beerOrder);
+		
+		sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATE_ORDER);
+		
+		return savedBeerOrder;
+	}
+
+	private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEventEnum beerOrderEventEnum) {
+
+		StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> sm = build(beerOrder);
+
+		Message<BeerOrderEventEnum> message = MessageBuilder.withPayload(beerOrderEventEnum)
+				.setHeader(ORDER_ID_HEADER, beerOrder.getId()).build();
+
+		sm.sendEvent(message);
+
+	}
+
+	private StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> build(BeerOrder beerOrder) {
+
+		StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> sm = stateMachineFactory
+				.getStateMachine(beerOrder.getId());
+		sm.stop();
+
+		sm.getStateMachineAccessor().doWithAllRegions(sma -> {
+			sma.addStateMachineInterceptor(beerOrderStateChangeInterceptor);
+			sma.resetStateMachine(new DefaultStateMachineContext<BeerOrderStatusEnum, BeerOrderEventEnum>(
+					beerOrder.getOrderStatus(), null, null, null));
+		});
+
+		sm.start();
+
+		return sm;
+	}
+
+}
